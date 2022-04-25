9fc8470db8fef16d006aee310c63e61972c1fffe
peteroskovich <79411486+peteroskovich@users.noreply.github.com>
2022-04-25 18:41:46 +0200

    Add files via upload

---
+++ src/main/java/com/ashop/shop/controllers/ShopApplication.java
@@ -0,0 +1,18 @@
+package com.ashop.shop.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class ShopApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
