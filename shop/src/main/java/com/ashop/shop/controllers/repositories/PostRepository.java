ef33c1d3d33b637d8906386b57a78a48da3268c4
peteroskovich <79411486+peteroskovich@users.noreply.github.com>
2022-04-25 21:40:40 +0500

    Delete shop directory

--- shop/src/main/java/com/ashop/shop/controllers/repositories/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ashop.shop.controllers.repositories;
-
-import com.ashop.shop.controllers.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post,Long> {
-}
