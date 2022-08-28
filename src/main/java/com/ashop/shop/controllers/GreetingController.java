83d36e5f444e7b207683c58f74531b3473bcd7ae
peteroskovich <peter.sil@outlook.it>
2022-08-28 22:34:14 +0200

    book view button functionality add remove and edit functions

--- src/main/java/com/ashop/shop/controllers/GreetingController.java
+++ src/main/java/com/ashop/shop/controllers/GreetingController.java
@@ -1,13 +1,18 @@
 package com.ashop.shop.controllers;
 
 import com.ashop.shop.models.Book;
+import com.ashop.shop.models.Post;
 import com.ashop.shop.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class GreetingController {
@@ -21,9 +26,10 @@ public class GreetingController {
         Iterable<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
 //        model.addAttribute("name", name);
-        model.addAttribute("name", "home page");
+//        model.addAttribute("name", "home page");
         return "greeting";
     }
+
     @GetMapping("/book/add")
     public String bookAdd(Model model) {
         return "book-add";
@@ -31,7 +37,7 @@ public class GreetingController {
 
 
     @PostMapping("/book/add")
-    public String blogPostAdd(@RequestParam String bookTitle, @RequestParam String description, @RequestParam String img) {
+    public String bookPostAdd(@RequestParam String bookTitle, @RequestParam String description, @RequestParam String img) {
         Book book = new Book(bookTitle, description, img);
         bookRepository.save(book);
         return "redirect:/";
@@ -46,5 +52,47 @@ public class GreetingController {
         return "support";
     }
 
+    @GetMapping("/book/{id}")
+    public String bookDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!bookRepository.existsById(id)) {
+            return "redirect:/book";
+        }
 
+        Optional<Book> book = bookRepository.findById(id);
+        ArrayList<Book> res = new ArrayList<>();
+        book.ifPresent(res::add);
+        model.addAttribute("book", res);
+        return "book-details";
+    }
+    @GetMapping("/book/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!bookRepository.existsById(id)) {
+            return "redirect:/book";
+        }
+        Optional<Book> book = bookRepository.findById(id);
+        ArrayList<Book> res = new ArrayList<>();
+        book.ifPresent(res::add);
+        model.addAttribute("book", res);
+        return "book-edit";
+    }
+
+    @PostMapping("/book/{id}/edit")
+    public String bookPostUpdate(@PathVariable(value = "id") long id, @RequestParam String bookTitle, @RequestParam String description, @RequestParam String img) {
+        Book book = bookRepository.findById(id).orElseThrow();
+        book.setBookTitle(bookTitle);
+        book.setDescription(description);
+        book.setImg(img);
+        bookRepository.save(book);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/book/{id}/remove")
+    public String bookPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Book book = bookRepository.findById(id).orElseThrow();
+        bookRepository.delete(book);
+
+        return "redirect:/";
+
+    }
 }
